b55af05225c9f1fa17784e48eff76b63726c314e
martins <luotianwen456123@126.com>
2019-08-12 14:40:20 +0800

    init

---
+++ src/main/java/com/order/www/orderInterface/InterfaceController.java
@@ -0,0 +1,19 @@
+package com.order.www.orderInterface;
+
+
+import com.jfinal.core.Controller;
+/**
+
+ */
+
+public class InterfaceController extends Controller {
+	
+
+	
+	public void index() {
+		 renderJson("a");
+	}
+
+}
+
+
